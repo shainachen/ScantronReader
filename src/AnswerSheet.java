385d6832ac78aad1de2f27c5ec405e53c46391eb
schen625 <schen625@fhs.win.fuhsd.org>
2017-03-08 10:25:08 -0800

    Add method to calculate scantron statistics

--- src/AnswerSheet.java
+++ src/AnswerSheet.java
@@ -4,31 +4,36 @@ import java.util.ArrayList;
  * A class to represent a set of answers from a page
  */
 public class AnswerSheet {
+	
 	private ArrayList<String> answers;
 	
 	public AnswerSheet(){
-		answers=new ArrayList<String>();
-	}
-
-	public ArrayList<String> getAnswers() {
-		return answers;
+		answers= new ArrayList<String>();
 	}
 	
-	public String getAnswer(int index) {
-		return answers.get(index);
+	public void addAnswer(int answer){
+		if (answer == 0) answers.add("a");
+		else if (answer == 1) answers.add("b");
+		else if (answer == 2) answers.add("c");
+		else if (answer == 3) answers.add("d");
+		else if (answer == 4) answers.add("e");
 	}
-
-	public void setAnswers(ArrayList<String> answers) {
-		this.answers = answers;
+	
+	public String getAnswer(int index){
+		return answers.get(index);
 	}
 	
 	public void addAnswer(String answer){
 		answers.add(answer);
 	}
-	
-//	public void removeAnswer(String answer){
-//		for(int i=0; i<answers.size(); i++)
-//			if(answer.equals(answers.get(i)))
-//				answers.remove(i);
-//	}
+
+	public void print(){
+		for(int i = 0; i < answers.size(); i++)
+			System.out.print(answers.get(i) + " ");
+		System.out.println();
+	}
+
+	public int getNumQuestions() {
+		return answers.size();
+	}
 }
--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import processing.core.PImage;
 public class Main {
     public static final String PDF_PATH = "/omrtest.pdf";
     public static OpticalMarkReader markReader = new OpticalMarkReader();
-
+    public static int[][] statistics;
     public static void main(String[] args) {
 	System.out.println("Welcome!  I will now auto-score your pdf!");
 	System.out.println("Loading file..." + PDF_PATH);
@@ -15,7 +15,6 @@ public class Main {
 	scoreAllPages(images);
 
 	System.out.println("Complete!");
-
 	// Optional: add a saveResults() method to save answers to a csv file
     }
 
@@ -29,17 +28,54 @@ public class Main {
      *            List of images corresponding to each page of original pdf
      */
     private static void scoreAllPages(ArrayList<PImage> images) {
-	ArrayList<AnswerSheet> scoredSheets = new ArrayList<AnswerSheet>();
+    	statistics=new int[images.size()][4];
+    	ArrayList<AnswerSheet> scoredSheets = new ArrayList<AnswerSheet>();
 
 	// Score the first page as the key
-	AnswerSheet key = markReader.processPageImage(images.get(0));
+    	AnswerSheet key = markReader.processPageImage(images.get(0));
+		calculateStatistics(key, key, 0);
 
-	for (int i = 1; i < images.size(); i++) {
-	    PImage image = images.get(i);
 
-	    AnswerSheet answers = markReader.processPageImage(image);
-
-	    // do something with answers
-	}
+    	for (int i = 1; i < images.size(); i++) {
+    		PImage image = images.get(i);
+    		AnswerSheet answers = markReader.processPageImage(image);
+    		calculateStatistics(answers, key, i);
+    	}
     }
+    private static void calculateStatistics(AnswerSheet answers, AnswerSheet key, int arrRow){
+    	int numCorrect=0, numIncorrect=0;
+    	double percentCorrect=0, percentIncorrect=0;
+    	int numAnswers=answers.getNumQuestions();
+    	for(int i=0; i< numAnswers; i++)
+    		if(answers.getAnswer(i).equals(key.getAnswer(i)))
+    			numCorrect++;
+    	numIncorrect=numAnswers-numCorrect;
+    	percentCorrect=((double)numCorrect/(double)numAnswers)*100;
+    	percentIncorrect=100-percentCorrect;
+    	statistics[arrRow][0]=numCorrect;
+    	statistics[arrRow][1]=numIncorrect;
+    	statistics[arrRow][2]=(int) percentCorrect;
+    	statistics[arrRow][3]=(int) percentIncorrect;
+    }
+    private static void saveResults(){
+    	
+    }
+    
+//    private void Form1_Load(object sender, EventArgs e){
+//       double[][] data = new double[10000][650];
+//       using (StreamWriter outfile = new StreamWriter(@"C:\Temp\test.csv"))
+//       {
+//          for (int x = 0; x < 10000; x++)
+//          {
+//             string content = "";
+//             for (int y = 0; y < 650; y++)
+//             {
+//                content += data[x][y].ToString("0.00") + ";";
+//             }
+//             outfile.WriteLine(content);
+//          }
+//       }
+//    }
+    
+    
 }
