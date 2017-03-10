dc3a65a5a93fe9285a7a928fb04bbebb4d7b8d5a
Shaina Chen <shaina.sierra@gmail.com>
2017-03-09 20:49:27 -0800

    Add JavaDoc comments to methods

--- src/CSVData.java
+++ src/CSVData.java
@@ -1,102 +1,172 @@
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
-public class CSVData {
-	private static boolean DEBUG = false;
-	private double[][] rawData;
+import processing.core.PImage;
 
-	private CSVData(double[][] data) {
-		rawData = data;
-	}
-	
-	public static CSVData createDataSet(String filepath, int linesToSkip) {
-		debug("Reading file: " + filepath);
-		
-		String data = readFileAsString(filepath);
-		String[] lines = data.split("\n");
-		
-		debug("Reading " + lines.length + " total lines from file");
-		debug("Using index " + (linesToSkip) + " as header row");
-		
-		String headerLine = lines[linesToSkip];
-		debug("Headers: " + headerLine);
-		
-		String[] headers = headerLine.split(",");
-		debug("Parsed header line into: " + headers.length + " total columns");
-		
-		int startColumn = 0;
-		return createDataSet(filepath, linesToSkip + 1, headers, startColumn);
-	}
-	
-	public static CSVData createDataSet(String filepath, int linesToSkip, String[] columnHeaders, int startColumn) {
-		debug("Reading file: " + filepath);
-		
-		String data = readFileAsString(filepath);
-		String[] lines = data.split("\n");
-		
-		debug("Reading " + lines.length + " total lines from file");
-		
-		int numColumns = columnHeaders.length;
-		debug("Reading " + numColumns + " total columns");
-		
-		int startRow = linesToSkip;
-		
-		// create storage for data
-		double[][] numdata = new double[lines.length - linesToSkip][numColumns];
+public class Main {
+    public static final String PDF_PATH = "/omrtest.pdf";
+    public static OpticalMarkReader markReader = new OpticalMarkReader();
+    private static int[][] statistics;
+	private static int[][] itemAnalysis;
+	private static String[] statTitles={"page number", "num correct", "num incorrect", "% correct", "% incorrect"};
+	private static String[] statRowTitles={"answer key", "1", "2", "3", "4", "5", "6"};
+	private static String[] itemTitles={"problem #", "# students that got it wrong", "% students that got it wrong", "percentages beyond acceptable % of students who got it wrong (>50%)"};
+	private static int percentWrongThreshold=50;
 
-		for (int r = startRow; r < lines.length; r++) {
-			String line = lines[r];
-			String[] coords = line.split(",");
+    public static void main(String[] args) {
+    	System.out.println("Welcome!  I will now auto-score your pdf!");
+    	System.out.println("Loading file..." + PDF_PATH);
+    	ArrayList<PImage> images = PDFHelper.getPImagesFromPdf(PDF_PATH);
 
-			for (int j = startColumn; j < numColumns; j++) {
-				if (coords[j].endsWith("#")) coords[j] = coords[j].substring(0, coords[j].length()-1);
-				double val = Double.parseDouble(coords[j]);
-				numdata[r - 1][j - startColumn] = val;
-			}
-		}
+    	System.out.println("Scoring all pages...");
+    	scoreAllPages(images);
 
-		return new CSVData(numdata);
-	}
-	
-	public double[][] getAllData() {
-		return rawData;
-	}
-	
-	
+    	System.out.println("Complete!");
+    }
+
+    /***
+     * Score all pages in list, using index 0 as the key.
+     * 
+     * NOTE: YOU MAY CHANGE THE RETURN TYPE SO YOU RETURN SOMETHING IF YOU'D
+     * LIKE
+     * 
+     * @param images
+     *            List of images corresponding to each page of original pdf
+     */
+    private static void scoreAllPages(ArrayList<PImage> images) {
+    	statistics=new int[images.size()][4];
+//    	ArrayList<AnswerSheet> scoredSheets = new ArrayList<AnswerSheet>();
+
+    	// Score the first page as the key
+    	AnswerSheet key = markReader.processPageImage(images.get(0));
+    	itemAnalysis=new int[key.getNumQuestions()][4];
+
+		calculateStatistics(key, key, 0);
 		
-	public static void writeDataToFile(String filePath, String data) {
-		File outFile = new File(filePath);
+		//calculate statistics for each page after the answer key
+    	for (int i = 1; i < images.size(); i++) {
+    		PImage image = images.get(i);
+    		AnswerSheet answers = markReader.processPageImage(image);
+    		calculateStatistics(answers, key, i);
+    	}
+    	
+    	//calculate percent students who got a problem wrong
+    	for(int i=0; i<itemAnalysis.length; i++){
+    		itemAnalysis[i][2]=(itemAnalysis[i][1]*100)/(images.size()-1);
+    		if(itemAnalysis[i][2]>percentWrongThreshold)
+    			itemAnalysis[i][3]=itemAnalysis[i][2];
+    	}
+    	
+    	String stats=saveResults(statTitles, statRowTitles, intToString(statistics), ", ");
+    	String items=saveResults(itemTitles, intToString(itemAnalysis), ", ");
 
-		try (BufferedWriter writer = new BufferedWriter(new FileWriter(outFile))) {
-			writer.write(data);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static String readFileAsString(String filepath) {
-		StringBuilder output = new StringBuilder();
-
-		try (Scanner scanner = new Scanner(new File(filepath))) {
-
-			while (scanner.hasNext()) {
-				String line = scanner.nextLine();
-				output.append(line + System.getProperty("line.separator"));
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		return output.toString();
-	}
-	
-	
-	private static void debug(String string) {
-		if (DEBUG ) {
-			System.err.println(string);
-		}
-	}
+    	CSVData.writeDataToFile("E:\\Shaina\\11th grade\\AP Comp Sci\\scantronStats.csv", stats);
+    	CSVData.writeDataToFile("E:\\Shaina\\11th grade\\AP Comp Sci\\scantronItemAnalysis.csv", items);
+    }
+    /**
+     * Calculates statistics (# correct, # incorrect, % correct, % incorrect for each page) and item analysis (number of students that got the problem wrong, % of students got the problem wrong)
+     * for each answer sheet by comparing it to the key.
+     * @param answers page in PDF which represents the student answers
+     * @param key first page of PDF which represents the scantron answer key
+     * @param arrRow row in the statistics 2D array (which page the current statistics are for, eg. answer key, page 1, etc.)
+     */
+    private static void calculateStatistics(AnswerSheet answers, AnswerSheet key, int arrRow){
+    	int numCorrect=0, numIncorrect=0;
+    	double percentCorrect=0, percentIncorrect=0;
+    	int numAnswers=answers.getNumQuestions();
+    	for(int i=0; i< numAnswers; i++){
+    		itemAnalysis[i][0]=i+1;
+    		if(answers.getAnswer(i).equals(key.getAnswer(i)))
+    			numCorrect++;
+    		else{
+    			System.out.println("Problem "+(i+1)+" is incorrect. ("+answers.getAnswer(i)+", "+key.getAnswer(i)+")");
+    			itemAnalysis[i][1]+=1;
+    		}
+    	}
+    	numIncorrect=numAnswers-numCorrect;
+    	percentCorrect=((double)numCorrect/(double)numAnswers)*100;
+    	System.out.println("percent correct: "+percentCorrect);
+    	percentIncorrect=100-percentCorrect;
+    	
+    	//enter in statistics values
+    	statistics[arrRow][0]=numCorrect;
+    	statistics[arrRow][1]=numIncorrect;
+    	statistics[arrRow][2]=(int) percentCorrect;
+    	statistics[arrRow][3]=(int) percentIncorrect;
+    }
+    
+    /**
+     * Transfers int 2D array items to String 2D array
+     * @param arr int 2D array
+     * @return String 2D array, representing the int 2D array's values cast as Strings
+     */
+    private static String[][] intToString(int[][] arr){
+    	String[][] a=new String[arr.length][arr[0].length];
+    	for(int i=0; i<arr.length; i++)
+    		for(int j=0; j<arr[0].length; j++)
+    			a[i][j]=Integer.toString(arr[i][j]);	
+    	return a;
+    }
+    
+    /**
+     * Transfers all 1D and 2D Strings into a single String variable to write as a CSV file
+     * @param firstCol column of Strings to insert in the first column of the CSV file
+     * @param in input 2D String of the data
+     * @param delim separator value 
+     * @return String of all combined String[] and String[][], to be used to write as a CSV file
+     */
+    private static String saveResults(String[] firstCol, String[][] in, String delim){
+            String out = "";
+            for(int i=0; i<firstCol.length; i++){
+            	out+=(firstCol[i]);
+            	if(i!=firstCol.length-1)
+        			out += delim;
+            }
+            out+=("\n");
+            for(int x = 0;x < in.length;x++){
+            	for(int y = 0;y < in[x].length;y++){
+            		if(y==in[x].length-1)
+            			if(!in[x][y].equals("0"))
+            				out+=(in[x][y]);
+            		else
+            			out += (in[x][y]);
+            		if(y!=in[x].length-1)
+            			out += delim;
+            	}
+            	out += ("\n");
+            }
+            return(out);
+       }
+    
+    /**
+     * Specialized version of above saveResults. Includes a firstRow, which inserts the firstRow string into the CSV data as the first row.
+     * @param firstCol column of Strings to insert in the first column of the CSV file
+     * @param firstRow row of Strings to insert in the first row of the CSV file
+     * @param in input 2D String of the data
+     * @param delim separator value 
+     * @return String of all combined String[] and String[][], to be used to write as a CSV file
+     */
+    private static String saveResults(String[] firstCol, String[] firstRow, String[][] in, String delim){
+    	 String out = "";
+         for(int i=0; i<firstCol.length; i++){
+         	out+=(firstCol[i]);
+         	if(i!=firstCol.length-1)
+     			out += delim;
+         }
+         out+=("\n");
+         for(int x = 0;x < in.length;x++){
+         	for(int y = 0;y < in[x].length;y++){
+         		if(y==0){
+         			out+=(firstRow[x]);
+         			out += delim;
+         		}
+         		out += (in[x][y]);
+         		if(y!=in[x].length-1)
+         			out += delim;
+         	}
+         	out += ("\n");
+         }
+         return(out);
+    }
 }
